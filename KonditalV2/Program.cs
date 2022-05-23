84e4bd08d6ba5eeb408ec3a36bca73205e1259ab
Emil <72310789+WhySoShy@users.noreply.github.com>
2022-05-23 23:21:43 +0200

    Tilføjelse af CalcKondital

--- KonditalV2/Program.cs
+++ KonditalV2/Program.cs
@@ -16,10 +16,15 @@ namespace KonditalV2
             int maxpuls = Convert.ToInt32(Console.ReadLine());
             Console.Write("\nIndtast din vægt: ");
             double vægt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Kondital.CalcKondital(puls, maxpuls, vægt));
         }
     }
     class Kondital
     {
-
+        public static string CalcKondital(int puls, int maxpuls, double vægt)
+        {
+            double kondital = maxpuls / puls * 15.3, maxilt = kondital * vægt / 1000;
+            return "Kondital: " + (int)kondital + "\nMax iltoptagelse: " + (int)maxilt + " liter";
+        }
     }
 }

8b3f44525eae48b8f9d0fc55d57bc0b03fe0cbcc
Emil Paaske Kvandal <emil875r@elevcampus.dk>
2022-05-23 09:12:39 +0200

    Tilføjelse af regning af kondital

--- Kondital/Program.cs
+++ Kondital/Program.cs
@@ -14,12 +14,20 @@ namespace Kondital
             int puls = Convert.ToInt32(Console.ReadLine());
             Console.Write("\nIndtast din max puls: ");
             int maxpuls = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nIndtast din vægt: ");
+            int vægt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Main(puls, maxpuls, vægt));
 
+        }
+        static int Main(int puls, int maxpuls, int vægt)
+        {
+            int kondital = maxpuls / puls * (int)15.3;
+            return kondital * vægt / 1000;
         }
     }
 }
 
-class Kondital
+class KonditalNr2
 {
 
 }

b2f40fa430ea49bb69aad2d904f8c5b05c0fbb23
Emil <72310789+WhySoShy@users.noreply.github.com>
2022-05-23 23:18:05 +0200

    Indlæsning af bruger data

--- KonditalV2/Program.cs
+++ KonditalV2/Program.cs
@@ -10,6 +10,12 @@ namespace KonditalV2
     {
         static void Main(string[] args)
         {
+            Console.Write("Indtast din puls: ");
+            int puls = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nIndtast din max puls: ");
+            int maxpuls = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nIndtast din vægt: ");
+            double vægt = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
